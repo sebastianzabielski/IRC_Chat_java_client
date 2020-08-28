1751b801a6bc0e51944b525be38fab21cdccb9ac
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2020-08-28 14:20:12 +0200

    Working on Receive class.

--- src/main/java/dataStructures/Receive.java
+++ src/main/java/dataStructures/Receive.java
@@ -1,14 +1,46 @@
 package dataStructures;
 
+import error.ReadonlyAttributeException;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.HashMap;
 import java.util.Map;
 
-public class Recieve {
-    private final Map<String, Object> header;
-    private final Map<String, Object> body;
+public class Receive {
+    static Map<String, Object> parseStringToMap(String string) {
+        throw new NotImplementedException();
+    }
 
-    public Recieve(Map<String, Object> header, Map<String, Object> body){
+    private final Map<String, Object> header;
+    private Map<String, Object> body = null;
+
+    public Receive(Map<String, Object> header) {
         this.header = header;
-        this.body = body;
+    }
+
+    public Receive(Map<String, Object> header, Map<String, Object> body) throws ReadonlyAttributeException {
+        this(header);
+        this.setBody(body);
+    }
+
+    public Receive(String header) {
+        this(Receive.parseStringToMap(header));
+    }
+
+    public Receive(String header, String body) throws ReadonlyAttributeException {
+        this(Receive.parseStringToMap(header), Receive.parseStringToMap(body));
+    }
+
+    public void setBody(Map<String, Object> body) throws ReadonlyAttributeException {
+        if (body != null) {
+            throw new ReadonlyAttributeException();
+        } else {
+            this.body = body;
+        }
+    }
+
+    public void setBody(String body) {
+        this.body = Receive.parseStringToMap(body);
     }
 
     public Map<String, Object> getHeader() {
