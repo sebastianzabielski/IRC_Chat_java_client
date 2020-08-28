31220a0437d69ee1201b5668cc24ac821ca4298b
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2020-08-28 15:11:58 +0200

    Add ApiStatics class.

--- src/main/java/statics/ApiStatics.java
+++ src/main/java/statics/ApiStatics.java
@@ -1,2 +1,14 @@
-package statics;public class ApiStatics {
+package statics;
+
+public class ApiStatics {
+    public static final String IOSeparator = "\0";
+    public static final String IOBlockSeparator = IOSeparator + IOSeparator;
+    public static final String KeyValueSeparator = ":";
+
+    public enum Keys {
+        ConLen,
+        Action,
+        Token,
+        Status,
+    }
 }
