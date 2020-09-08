a38dece71e58347fcac93096deea5624058f565a
Sebastian Zabielski <sebastian.zabielski16@gmail.com>
2020-09-08 19:18:49 +0200

    Working on CLient class.

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Client {
+import dataStructures.Receive;
+import exceptions.ReadonlyAttributeException;
+
+import java.io.IOException;
+
+public final class Client {
+    private static Client instance;
+    private Connection connection;
+
+    public Client(String ip, int port) throws IOException {
+        if(this.connection == null) {
+            this.connection = new Connection(ip, port);
+            //TODO run listenReceive on another thread
+
+        }
+    }
+
+    private void listenReceive() {
+        while(true) {
+            try {
+                Receive receive = this.connection.receive();
+                //TODO emit receive value
+            } catch (IOException e) {
+                System.out.println("ioexception");
+                //TODO need to find a way to clear buffer when pat of data is wrong
+                //maby send to backend for recerate message, and clear buffer
+                e.printStackTrace();
+            } catch (ReadonlyAttributeException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        //TODO stop thread
+
+        super.finalize();
+    }
 }
